299488fb97db110d92183514c8ae1775f935e9dc
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2025-05-28 15:52:38 +0500

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-green-100">
+      <h1 className="text-4xl font-bold">🐄 FarmConnect</h1>
+      <p className="text-lg">Digitizing livestock ownership & health in Pakistan</p>
+    </div>
+  );
+}
+
+export default App;
